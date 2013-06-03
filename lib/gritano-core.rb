0b0f2304acb7fee2453229e6b877ecbe4a3c144a
Ígor Bonadio <igorbonadio@gmail.com>
2013-06-03 14:49:36 -0300

    project's gems

---
+++ lib/gritano-core.rb
@@ -0,0 +1,6 @@
+ROOT_PATH = File.expand_path(File.dirname(__FILE__))
+
+# require File.join(ROOT_PATH, 'gritano-core/user')
+# require File.join(ROOT_PATH, 'gritano-core/repository')
+# require File.join(ROOT_PATH, 'gritano-core/permission')
+# require File.join(ROOT_PATH, 'gritano-core/key')
