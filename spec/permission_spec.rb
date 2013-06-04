c26d4d208b91679f7dffd403b7f3a6cc1988e983
Ígor Bonadio <igorbonadio@gmail.com>
2013-06-04 15:36:47 -0300

    more specs for permissions

--- spec/permission_spec.rb
+++ spec/permission_spec.rb
@@ -14,23 +14,42 @@ module Gritano::Core
       Permission.new(contributor_id: 1, repository_id: 1).should be_invalid
     end
 
-    it "should manage READ access" do
+    it "should add READ access" do
       permission = Permission.new(contributor_id: 1, repository_id: 1)
       permission.add_access(:read)
       permission.access.should be == Permission::READ
     end
 
-    it "should manage WRITE access" do
+    it "should add WRITE access" do
       permission = Permission.new(contributor_id: 1, repository_id: 1)
       permission.add_access(:write)
       permission.access.should be == Permission::WRITE
     end
 
-    it "should manage WRITE access" do
+    it "should add WRITE access" do
       permission = Permission.new(contributor_id: 1, repository_id: 1)
       permission.add_access(:write)
       permission.add_access(:read)
       permission.access.should be == Permission::WRITE | Permission::READ
+    end
+
+    it "should remove READ access" do
+      permission = Permission.new(contributor_id: 1, repository_id: 1, access: 3)
+      permission.remove_access(:read)
+      permission.access.should be == Permission::WRITE
+    end
+
+    it "should remove WRITE access" do
+      permission = Permission.new(contributor_id: 1, repository_id: 1, access: 3)
+      permission.remove_access(:write)
+      permission.access.should be == Permission::READ
+    end
+
+    it "should remove WRITE access" do
+      permission = Permission.new(contributor_id: 1, repository_id: 1, access: 3)
+      permission.remove_access(:write)
+      permission.remove_access(:read)
+      permission.access.should be == 0
     end
   end
 end
