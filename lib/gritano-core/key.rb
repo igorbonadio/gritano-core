5017935729632bb584ea816f9413174867ef07bd
Ígor Bonadio <igorbonadio@gmail.com>
2013-06-05 14:41:46 -0300

    key should be correctly formatted

--- lib/gritano-core/key.rb
+++ lib/gritano-core/key.rb
@@ -5,7 +5,12 @@ module Gritano
       validates :name, uniqueness: {scope: :user_id}
 
       validates :key, presence: true
+      validates :key, format: /\Assh-(?:dss|rsa) .* /
       validates :user_id, presence: true
+
+      def key=(pubkey)
+        write_attribute(:key, pubkey.scan(/\Assh-(?:dss|rsa) .* /)[0][0..-2])
+      end
     end
   end
 end
