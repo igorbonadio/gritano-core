56516cc4e2f0e05b1c9c7f2c1a5b6d07edf9ebf5
Ígor Bonadio <igorbonadio@gmail.com>
2013-06-08 15:21:11 -0300

    root_path bug

--- lib/gritano-core.rb
+++ lib/gritano-core.rb
@@ -1,10 +1,10 @@
-ROOT_PATH = File.expand_path(File.dirname(__FILE__))
+root_path = File.expand_path(File.dirname(__FILE__))
 
 require 'active_record'
 require 'grit'
 
-require File.join(ROOT_PATH, 'gritano-core/user')
-require File.join(ROOT_PATH, 'gritano-core/repository')
-require File.join(ROOT_PATH, 'gritano-core/permission')
-require File.join(ROOT_PATH, 'gritano-core/key')
-require File.join(ROOT_PATH, 'gritano-core/migration')
+require File.join(root_path, 'gritano-core/user')
+require File.join(root_path, 'gritano-core/repository')
+require File.join(root_path, 'gritano-core/permission')
+require File.join(root_path, 'gritano-core/key')
+require File.join(root_path, 'gritano-core/migration')
