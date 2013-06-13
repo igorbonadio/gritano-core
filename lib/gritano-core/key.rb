0b2e3dc933854ebac02a2a673017fe1492377689
Ígor Bonadio <igorbonadio@gmail.com>
2013-06-12 22:59:25 -0300

    adding key validation

--- lib/gritano-core/key.rb
+++ lib/gritano-core/key.rb
@@ -5,10 +5,11 @@ module Gritano
       validates :name, uniqueness: {scope: :user_id}
 
       validates :key, presence: true
+      validates :key, format: /\Assh-(?:dss|rsa) .*/
       validates :user_id, presence: true
 
       def key=(pubkey)
-        write_attribute(:key, pubkey.scan(/\Assh-(?:dss|rsa) .* /)[0][0..-2])
+        write_attribute(:key, pubkey.scan(/\Assh-(?:dss|rsa) .*/)[0][0..-2])
       end
     end
   end
